0749ba6a68ac5b1b2ca7e79b03073faef7d55cc6
Alexandre Perdomo <aperdomo@free.fr>
2021-02-25 12:16:39 +0100

    configure jquery in webpack

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_24_164025) do
+ActiveRecord::Schema.define(version: 2021_02_25_095737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "link_visits", force: :cascade do |t|
+    t.bigint "link_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["link_id"], name: "index_link_visits_on_link_id"
+  end
 
   create_table "links", force: :cascade do |t|
     t.string "url", null: false
